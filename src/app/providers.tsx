f03992cb762a9619d96ca75f54fa05e9f61c9653
matisantillan11 <matias1.santillan@gmail.com>
2025-09-07 16:30:52 -0300

    use dark theme as default

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider defaultTheme="light" attribute="class">
+    <ThemeProvider defaultTheme="dark" attribute="class">
       <AuthProvider>
         <SidebarProvider>{children}</SidebarProvider>
       </AuthProvider>
